c17d6acfa83c1c2f8bc30cf419ff8eef92cab005
Michael Crawford <michael.crawford92@gmail.com>
2021-02-17 09:54:46 -0800

    initial

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+class AboutPage extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+    this.elements = {}
+  }
+  render() {
+    return (
+      <h1>h1asd</h1>
+    )
+  }
+}
+
+export default AboutPage
